fc512a60473804b15bb96ae43f882ab3f83bcf54
ydong <1094035520@qq.com>
2018-12-12 10:58:24 +0800

    dev:修改retrofit的请求GET POST URL注解 结合Gank.io.api

--- app/src/multithreading/java/com/flavors/xiaomi/RxJavaAndRetrofit/GankApi.java
+++ app/src/multithreading/java/com/flavors/xiaomi/RxJavaAndRetrofit/GankApi.java
@@ -1,8 +1,13 @@
 package com.flavors.xiaomi.RxJavaAndRetrofit;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Url;
 
 public interface GankApi {
 
@@ -11,4 +16,16 @@ public interface GankApi {
                            @Path("size") String size,
                            @Path("page") String page);
 
+    @GET
+    Call<GankBean> getDataWithUrl(@Url String url);
+
+
+    @FormUrlEncoded
+    @POST("api/add2gank")
+    Call<ResponseBody> postData(@Field("url") String url,
+                                @Field("desc") String desc,
+                                @Field("who") String who,
+                                @Field("type") String type,
+                                @Field("debug") String debug
+    );
 }
--- app/src/multithreading/java/com/flavors/xiaomi/RxJavaAndRetrofit/RxRetrofitActivity.java
+++ app/src/multithreading/java/com/flavors/xiaomi/RxJavaAndRetrofit/RxRetrofitActivity.java
@@ -3,10 +3,16 @@ package com.flavors.xiaomi.RxJavaAndRetrofit;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import com.plat.sdk.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -17,39 +23,107 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * @author ydong
  */
 public class RxRetrofitActivity extends AppCompatActivity {
-
-    private Button gankAPiGet;
+    @BindView(R.id.gankAPiGet) Button gankAPiGet;
+    @BindView(R.id.gankAPiGetWithUrl) Button gankAPiGetWithUrl;
+    @BindView(R.id.gankAPiPOST) Button gankAPiPOST;
+    private Unbinder bind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_retrofit);
-        initView();
+        bind = ButterKnife.bind(this);
+
     }
 
-    private void initView() {
-        gankAPiGet = findViewById(R.id.gankAPiGet);
-        gankAPiGet.setOnClickListener(v -> {
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl("https://gank.io/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-            GankApi gankApi = retrofit.create(GankApi.class);
-            Call<GankBean> android = gankApi.getData("Android", "10", "1");
-            android.enqueue(new Callback<GankBean>() {
-                @Override
-                public void onResponse(Call<GankBean> call, Response<GankBean> response) {
-                    Log.d("ydong", "onResponse: " + response.body().getResults().get(0).getDesc());
-                }
+    @OnClick({R.id.gankAPiGet, R.id.gankAPiPOST, R.id.gankAPiGetWithUrl})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.gankAPiPOST:
+                gankPOSTRequest();
+                break;
+            case R.id.gankAPiGet:
+                gankGetRequest();
+                break;
+            case R.id.gankAPiGetWithUrl:
+                gankGetWithUrlRequest();
+                break;
+            default:
+                break;
 
-                @Override
-                public void onFailure(Call<GankBean> call, Throwable t) {
-                    Log.e("ydong", "onFailure: " + t);
+        }
+    }
 
-                }
-            });
+    private void gankGetRequest() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://gank.io/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        GankApi gankApi = retrofit.create(GankApi.class);
+        Call<GankBean> android = gankApi.getData("Android", "10", "1");
+        android.enqueue(new Callback<GankBean>() {
+            @Override
+            public void onResponse(Call<GankBean> call, Response<GankBean> response) {
+                Log.d("ydong", "onResponse: " + response.body().getResults().get(0).getDesc());
+            }
+
+            @Override
+            public void onFailure(Call<GankBean> call, Throwable t) {
+                Log.e("ydong", "onFailure: " + t);
+
+            }
         });
+    }
+
+    private void gankGetWithUrlRequest() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://gank.io/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        GankApi gankApi = retrofit.create(GankApi.class);
+        Call<GankBean> android = gankApi.getDataWithUrl("http://gank.io/api/data/Android/10/1");
+        android.enqueue(new Callback<GankBean>() {
+            @Override
+            public void onResponse(Call<GankBean> call, Response<GankBean> response) {
+                Log.d("ydong", "gankGetWithUrlRequest onResponse: " + response.body().getResults().get(0).getDesc());
+            }
+
+            @Override
+            public void onFailure(Call<GankBean> call, Throwable t) {
+                Log.e("ydong", "gankGetWithUrlRequest onFailure: " + t);
+
+            }
+        });
+    }
 
 
+    private void gankPOSTRequest() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://gank.io/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        GankApi gankApi = retrofit.create(GankApi.class);
+        Call<ResponseBody> responseBodyCall = gankApi.postData("http://square.github.io/retrofit/",
+                "测试数据",
+                "未来安卓大佬",
+                "Android",
+                "true");
+        responseBodyCall.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                Log.d("ydong", "gankPOSTRequest onResponse: " + response);
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("ydong", "gankPOSTRequest onResponse: " + t);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bind.unbind();
     }
 }
